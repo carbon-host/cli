64882fdfb863e329d0c56a71060e94ca267f481c
Dawson <hello@dawson.gg>
2024-10-27 17:14:04 -0400

    first commit

---
+++ src/commands.ts
@@ -0,0 +1,27 @@
+import yargs from "yargs"
+import {hideBin} from "yargs/helpers"
+
+yargs(hideBin(process.argv)).command(
+  "deploy <source> <target>",
+  "Deploys a file from the source (local) to the (server) target",
+  (yargs) => {
+    return yargs
+      .positional("source", {
+        description: "The source file on the local file system",
+        type: "string",
+      })
+      .positional("target", {
+        description: "The target file on the server",
+        type: "string",
+      });
+  }, (argv) => {
+    console.log(argv.source, argv.target);
+  })
+  .option("verbose", {
+    alias: "v",
+    type: "boolean",
+    description: "Enable verbose logging",
+  })
+  // show help if no command is provided
+  .demandCommand(1, "You need to specify a command")
+  .parse()
